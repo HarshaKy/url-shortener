bf81ac5e0452363378118d7c15aa36b1a78ed47d
HarshaKy <harshaky6573@gmail.com>
2019-01-29 22:54:43 +0530

    routes for PATCH url/:id

--- server/server.js
+++ server/server.js
@@ -55,6 +55,29 @@ app.get('/urls/:id', (req, res) => {
 });
 
 
+// PATCH a URL by ID
+app.patch('/urls/:id', (req, res) => {
+  var id = req.params.id;
+  var newTitle = req.body.title;
+  var newOriginalUrl = req.body.originalUrl;
+  var newTags = req.body.tags;
+  var body = _.pick(req.body, ['title', 'originalUrl', 'tags']);
+
+  // console.log(newTitle);
+  // console.log(body);
+
+  if (!ObjectId.isValid(id)) {
+    res.status(404).send('Not valid');
+  }
+
+  Url.findByIdAndUpdate(id, {$set: body}, {new: true}).then((url) => {
+    if (!url) {
+      return res.status(400).send();
+    }
+
+    res.send({url});
+  }).catch((e) => console.log('Not valid'));
+});
 
 app.listen(port, () => {
   console.log('Listening on port ' + port);
