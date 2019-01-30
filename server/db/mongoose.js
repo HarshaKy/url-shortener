749a3c3b67fe4c05e1e991789cae6d5fb4c136a1
HarshaKy <HarshaKy>
2019-01-30 13:56:53 +0530

    asdf

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 let db = {
   localhost: 'mongodb://localhost:27017/UrlShort',
-  mlab: 'mongodb://HarshaKy:fibonacci12358!@ds247410.mlab.com:47410/shrt'
+  mlab: 'mongodb://<username>:<password>@ds247410.mlab.com:47410/shrt'
 };
 mongoose.connect((process.env.PORT ? db.mlab : db.localhost), {useNewUrlParser: true});
 
